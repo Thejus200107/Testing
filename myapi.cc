8aff66b359251453ff588a2fb83c33b4d2460546
cthejus <cthejus2@gmail.com>
2024-01-09 23:10:13 -0800

    style: Added comment lines

--- myapi.cc
+++ myapi.cc
@@ -1,7 +1,6 @@
 #include "myapi.hh"
-
+//func definition
 int thejus(int a , int b){
-
 int c = a + b;
 return c;
 
--- myapi.hh
+++ myapi.hh
@@ -1,6 +1,8 @@
 #ifndef MYLIBRARY_H
 #define MYLIBRARY_H
 
+//func declaration
 int thejus(int a , int b);
 
 #endif
+
